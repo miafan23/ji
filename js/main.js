72d31aa85a4612c4469b642789967f7a1ab76c99
fiona23 <fiona_fanmy@163.com>
2015-10-09 17:02:27 +0800

    edit task and ouput or upload data

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ taskList.controller('taskListCtrl', function ($scope, localStorageService) {
     var allTasks;
 
     function init (){
+        //prevent select page while db click
+        clearSelection();
+
         var currentDate = new Date();
         var currentYear = currentDate.getFullYear();
         var currentMonth = currentDate.getMonth();
@@ -30,7 +33,6 @@ taskList.controller('taskListCtrl', function ($scope, localStorageService) {
             localStorageService.set('allTasks', []);
         }
         allTasks = localStorageService.get('allTasks');
-
     }
 
 
@@ -72,6 +74,7 @@ console.log('d')
             addedTask = new Task((startId + i), allTasksInput[i].innerHTML, date)
 
             allTasks[startId + i] = addedTask;
+            console.log(allTasks)
         }
         
         angular.element(allTasksInput).remove();
@@ -81,13 +84,8 @@ console.log('d')
 
     function updateFinishState (event) {
         var checbox = event.target;
-        // var state = checbox.checked;
 
-        var targetParentNode = checbox;
-
-        while(!targetParentNode.getAttribute('data-task')) {
-            targetParentNode = targetParentNode.parentNode;
-        }
+        var targetParentNode = findParent(checbox, 'data-task');
 
         var state = targetParentNode.getAttribute('ng-switch-when')
         var task = JSON.parse(targetParentNode.getAttribute('data-task'));
@@ -114,15 +112,145 @@ console.log('d')
 
         }
 
-        localStorageService.set('allTasks', allTasks)
+        updateStorage(allTasks)
 
+    }
+    
 
+    function outputData () {
+        var downloadLink = document.getElementById('output-data-link');
+        downloadLink.href = saveData();
+       function saveData () {
+            var textFile = null;
+            var text = JSON.stringify(localStorageService.get('allTasks'));
+
+            var data = new Blob([text], {type: 'text/plain'});
+            // If we are replacing a previously generated file we need to
+            // manually revoke the object URL to avoid memory leaks.
+            if (textFile !== null) {
+              window.URL.revokeObjectURL(textFile);
+            }
+
+            textFile = window.URL.createObjectURL(data);
+            
+            return textFile;
+        }
     }
 
 
+    function uploadData () {
+        var file = document.getElementById("input-data").files[0];
+        var r = new FileReader;
+
+        r.onloadend = function (e) {
+
+            var _allTasks = JSON.parse(e.target.result);
+            angular.copy(_allTasks, allTasks);
+            $scope.$apply();
+            updateStorage(allTasks)
+        }
+
+        r.readAsBinaryString(file)
+    }
+
+    function editTask (event) {
+
+        var target = event.target;
+        var taskTd = findParent(target, 'data-task');
+
+        var taskNameShow = taskTd.querySelector('.taskName');
+
+        var editWrapper = taskTd.querySelector('.edit-wrapper');
+        var taskInput = taskTd.querySelector('.edit-taskName-input');
+
+        editWrapper.style.display = 'block';
+        taskNameShow.style.display = 'none';
+
+    }
+
+    function deleteTask (event) {
+        var taskTd = findParent(event.target, 'data-task');
+
+        var task = JSON.parse(taskTd.getAttribute('data-task'))
+
+        var id = task.id;
+
+        allTasks[id] = {};
+
+        updateStorage(allTasks)
+    }
+
+    function finishEdit (event) {
+        var taskTd = findParent(event.target, 'data-task');
+        var task = JSON.parse(taskTd.getAttribute('data-task'));
+
+        var id = task.id;
+
+        allTasks[id].taskName = taskTd.querySelector('.edit-taskName-input').value;
+
+        var editWrapper = taskTd.querySelector('.edit-wrapper');
+        var taskNameShow = taskTd.querySelector('.taskName');
+
+        editWrapper.style.display = 'none';
+        taskNameShow.style.display = 'block';
+
+        updateStorage(allTasks)
+    }
+
+    function cancelEdit ($event) {
+        var taskTd = findParent(event.target, 'data-task');
+
+        var editWrapper = taskTd.querySelector('.edit-wrapper');
+        var taskNameShow = taskTd.querySelector('.taskName');
+
+        editWrapper.style.display = 'none';
+        taskNameShow.style.display = 'block';
+    }
+
+    function findParent (child, dataProp) {
+        var parentNode = child;
+
+        while (!parentNode.getAttribute(dataProp)) {
+            parentNode = parentNode.parentNode;
+        }
+
+        return parentNode
+    }
+
+    function updateStorage (allTasks) {
+        localStorageService.set('allTasks', allTasks)
+    }
+
+    function clearSelection() {
+        if(document.selection && document.selection.empty) {
+            document.selection.empty();
+        } else if(window.getSelection) {
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+        }
+    }
+
+    function filterEmpty (item) {
+        return !(item.id === undefined)
+    }
+
     $scope.allTasks = allTasks;
+
     $scope.enterTask = enterTask;
     $scope.finishEnterTasks = finishEnterTasks;
     $scope.updateFinishState = updateFinishState;
+
+    //about io data
+    $scope.outputData = outputData;
+    $scope.uploadData = uploadData;
+
+    //about edit task
+    $scope.editTask = editTask;
+    $scope.finishEdit = finishEdit;
+    $scope.deleteTask = deleteTask;
+    $scope.cancelEdit = cancelEdit;
+
+    //filter
+    $scope.filterEmpty = filterEmpty;
 
 })
