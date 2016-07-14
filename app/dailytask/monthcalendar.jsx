0ab063683c9bb5d53e945d44d2306cadd667469b
fiona23 <fiona_fanmy@163.com>
2016-07-14 15:14:41 +0800

    calendar

--- app/dailytask/monthcalendar.jsx
+++ app/dailytask/monthcalendar.jsx
@@ -1,21 +1,58 @@
 import './calendar.css';
 const React = require('react');
 const Pikaday = require('pikaday');
-const Moment = require('moment');
+const moment = require('moment');
 // import Calendar from 'rc-calendar';
 var CalendarPlugin = require('rc-calendar');
 var Calendar = React.createClass({
-  // getInitialState() {
-  //
-  // },
-  //
-  // componentDidMount() {
-  //   let picker = new Pikaday({})
-  // },
+  getInitialState() {
+    let now = moment();
+    //这个月有多少天
+    let dayNum = now.endOf('month').date();
+    let currentMonth = now.month()+1;
+    // let currentWeek = now.week();
+    let days = [];
+    for (let i = 1; i <= dayNum; i++) {
+      let thisDay = moment().date(i);
+      let isCurrentWeek = thisDay.week() === moment().week() && thisDay.year() === moment().year();
+      console.log(thisDay.week(), moment().week())
+      days.push({
+        date: i,
+        week: thisDay.day(),
+        //判断是不是和今天同一周
+        className: isCurrentWeek ? '' : 'mobile-hide'
+      });
+    }
+    console.log(days)
+    return {
+      now,
+      days,
+      currentMonth
+    }
+  },
+
+  componentDidMount() {
+    let picker = new Pikaday({})
+  },
 
   render() {
     return(
-      
+      <div>
+        <div>{this.state.currentMonth}月</div>
+        <div className="calendar-head">
+          {this.state.days.map((day) => {
+            return <div key={day.date} className={day.className}>{day.date}</div>
+          })}
+        </div>
+        <div className="calendar-week">
+          {this.state.days.map((day) => {
+            return <div key={day.date}>{day.week}</div>
+          })}
+        </div>
+        <div className="task">
+
+        </div>
+      </div>
     )
   }
 });
